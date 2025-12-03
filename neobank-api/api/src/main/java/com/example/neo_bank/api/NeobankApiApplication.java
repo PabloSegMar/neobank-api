ab2958f6b449bc46074fab3e95502a2d6b85b321
Pablo-Segura <psm0032@alu.medac.es>
2025-12-03 20:05:50 +0100

    He creado el proyecto de springboot

---
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/NeobankApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo_bank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeobankApiApplication.class, args);
+	}
+
+}
---
+++ neobank-api/api/src/test/java/com/example/neo_bank/api/NeobankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neo_bank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeobankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
