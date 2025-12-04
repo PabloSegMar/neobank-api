10c9a0c2447d85ac1815781c6ec927af40d40172
Pablo-Segura <psm0032@alu.medac.es>
2025-12-04 19:20:25 +0100

    Arreglado bug que hacia que no se sumara el dinero al cobrador

--- neobank-api/api/src/main/java/com/example/neo_bank/api/service/TransactionService.java
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/service/TransactionService.java
@@ -6,8 +6,8 @@ import com.example.neo_bank.api.model.Transaction;
 import com.example.neo_bank.api.model.TransactionType;
 import com.example.neo_bank.api.repository.AccountRepository;
 import com.example.neo_bank.api.repository.TransactionRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -22,38 +22,48 @@ public class TransactionService {
         this.transactionRepository = transactionRepository;
     }
 
-    // Esta anotación obliga a que se sume la cantidad del que recibe
-    // y se reste la cantidad del que paga
-    // si hay algún fallo en una de estas operaciones se deshace la acción que ya sé haya realizado
     @Transactional
     public void transferMoney(TransferRequest request) {
 
         // 1. Busco cuenta origen (la que paga)
-        Account fromAccount = accountRepository.findByIban(request.getFromIban()).orElseThrow(() -> new RuntimeException("La cuenta origen no ha sido encontrada"));
+        Account fromAccount = accountRepository.findByIban(request.getFromIban())
+                .orElseThrow(() -> new RuntimeException("La cuenta origen no ha sido encontrada"));
 
         // 2. Buscar cuenta destino (la que recibe)
-        Account toAccount = accountRepository.findByIban(request.getFromIban()).orElseThrow(() -> new RuntimeException("La cuenta destino no ha sido encontrada"));
+        Account toAccount = accountRepository.findByIban(request.getToIban())
+                .orElseThrow(() -> new RuntimeException("La cuenta destino no ha sido encontrada"));
 
-        // 3. Validar si hay saldo suficiente
-        // compareTo devuelve: -1 (menor), 0 (igual), 1 (mayor)
-
+        // 3. Validar saldo
         if (fromAccount.getBalance().compareTo(request.getAmount()) < 0) {
             throw new RuntimeException("No hay saldo suficiente");
         }
 
-        // 4. Restar a uno, Sumar a otro
-        // BigDecimal es inmutable, por tanto, tengo que guardar el resultado del .subtract()
-        BigDecimal newBalanceTo = toAccount.getBalance().subtract(request.getAmount());
-        fromAccount.setBalance(newBalanceTo);
+
+        // A. Restar al que paga
+        BigDecimal newBalanceFrom = fromAccount.getBalance().subtract(request.getAmount());
+        fromAccount.setBalance(newBalanceFrom);
+
+        // B. Sumar al que recibe
+        BigDecimal newBalanceTo = toAccount.getBalance().add(request.getAmount());
+        toAccount.setBalance(newBalanceTo);
 
         // 5. Guardar los cambios en BBDD
         accountRepository.save(fromAccount);
         accountRepository.save(toAccount);
 
-        //Recibo para el que paga (Dinero negativo)
+
+        // El que paga (Negativo)
+        Transaction debit = new Transaction();
+        debit.setAccount(fromAccount);
+        debit.setAmount(request.getAmount().negate()); // Restamos
+        debit.setType(TransactionType.TRANSFER);
+        debit.setTimestamp(LocalDateTime.now());
+        transactionRepository.save(debit);
+
+        // El que recibe (Positivo)
         Transaction credit = new Transaction();
         credit.setAccount(toAccount);
-        credit.setAmount(request.getAmount());
+        credit.setAmount(request.getAmount()); // Sumamos
         credit.setType(TransactionType.TRANSFER);
         credit.setTimestamp(LocalDateTime.now());
         transactionRepository.save(credit);
