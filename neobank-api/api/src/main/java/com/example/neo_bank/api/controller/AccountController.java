f3bfbbe9d60b5a0aafe3482a649f5d2d4d4bc797
Pablo-Segura <psm0032@alu.medac.es>
2025-12-04 17:44:05 +0100

    Creada la logica de AccountRepository

--- neobank-api/api/src/main/java/com/example/neo_bank/api/controller/AccountController.java
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/controller/AccountController.java
@@ -1,4 +1,27 @@
 package com.example.neo_bank.api.controller;
 
+import com.example.neo_bank.api.model.Account;
+import com.example.neo_bank.api.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    // Petición POST para crear cuenta a un usuario concreto
+    @PostMapping("/{userid}")
+    public ResponseEntity<Account> createAccount(@PathVariable Long userId) {
+        Account newAccount = accountService.createAccount(userId);
+        return ResponseEntity.ok(newAccount);
+    }
 }
--- neobank-api/api/src/main/java/com/example/neo_bank/api/repository/AccountRepository.java
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/repository/AccountRepository.java
@@ -1,4 +1,14 @@
 package com.example.neo_bank.api.repository;
 
-public class AccountRepository {
+import com.example.neo_bank.api.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    //Busca una cuenta por su IBAN
+    Optional<Account> findByIban(String iban);
+
+    boolean existsByIban(String id);
 }
