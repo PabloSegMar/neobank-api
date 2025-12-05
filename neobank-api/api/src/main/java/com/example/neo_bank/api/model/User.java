67abca3137fa695cd3b1cc7a67f2981c52c13da9
Pablo-Segura <psm0032@alu.medac.es>
2025-12-05 20:41:43 +0100

    Creados los metodo de verificacion de cuenta y credenciales

--- neobank-api/api/src/main/java/com/example/neo_bank/api/model/User.java
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/model/User.java
@@ -1,21 +1,26 @@
 package com.example.neo_bank.api.model;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity // 1. Convierte esta clase en una tabla SQL
-@Table(name= "users")  // 2. Nombre de la tabla en la base de datos
+@Table(name = "users")  // 2. Nombre de la tabla en la base de datos
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-    @Column (unique = true, nullable = false) // El email no puede repetirse ni estar vacio
+    @Column(unique = true, nullable = false) // El email no puede repetirse ni estar vacio
     private String email;
 
     private String password;
@@ -25,10 +30,42 @@ public class User {
 
     // Relación: Un usuario tiene muchas cuentas
     // "mappedBy" dice que la clave foránea está en la otra clase (Account)
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch =  FetchType.LAZY)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Account> accounts;
 
-    public User(){}
+    public User() {
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE " + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
     public Long getId() {
         return id;
