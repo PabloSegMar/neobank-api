2c4f7e6e69e32ef47148f601d5f62e2d7a5a3d8e
Pablo-Segura <psm0032@alu.medac.es>
2025-12-05 20:20:22 +0100

    Agregado getmapping para usuarios

--- neobank-api/api/src/main/java/com/example/neo_bank/api/controller/UserController.java
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/controller/UserController.java
@@ -1,27 +1,34 @@
 package com.example.neo_bank.api.controller;
 
 import com.example.neo_bank.api.model.User;
+import com.example.neo_bank.api.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.neo_bank.api.service.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    public ResponseEntity<User> createUser(@RequestBody User user) {
 
         User newUser = userService.registerUser(user);
         return ResponseEntity.ok(newUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser() {
+        return ResponseEntity.ok(userRepository.findAll());
     }
 }
