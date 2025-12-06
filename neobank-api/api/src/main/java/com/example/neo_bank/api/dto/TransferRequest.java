1fcd99d3e83bf09309c55c375e898729c972b44f
Pablo-Segura <psm0032@alu.medac.es>
2025-12-06 13:57:18 +0100

    Agregadas validaciones a la hora de hacer transferencias

--- neobank-api/api/src/main/java/com/example/neo_bank/api/dto/TransferRequest.java
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/dto/TransferRequest.java
@@ -1,13 +1,24 @@
 package com.example.neo_bank.api.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public class TransferRequest {
+    @NotBlank(message = "El IBAN de origen es obligatorio")
     private String fromIban; // El que paga
+
+    @NotBlank(message = "El IBAN de destino es obligatorio")
     private String toIban; // El que recibe
+
+    @NotNull(message = "Debes ingresar una cantidad obligatoriamente")
+    @DecimalMin(value = "0.01", message = "La cantidad mínima es 0.01")
     private BigDecimal amount; // Cantidad de dinero
 
-    public TransferRequest(){}
+    public TransferRequest() {
+    }
 
     public String getFromIban() {
         return fromIban;
