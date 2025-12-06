233ed1c6c2286aee972a6d281a3238f788daa6c3
Pablo-Segura <psm0032@alu.medac.es>
2025-12-06 18:09:41 +0100

    He creado el modelo de tarjeta y su enum

---
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/model/Card.java
@@ -0,0 +1,81 @@
+package com.example.neo_bank.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "cards")
+@AllArgsConstructor
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String cardNumber;
+
+    private String cvv;
+
+    private LocalDate expirationDate;
+
+    @Enumerated(EnumType.STRING)
+    private CardType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    public Card() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public void setType(CardType type) {
+        this.type = type;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
