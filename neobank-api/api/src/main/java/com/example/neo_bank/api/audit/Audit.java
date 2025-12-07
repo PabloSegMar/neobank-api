aa018380a5e62e4e2debe3ec8080ba102ef930f9
Pablo-Segura <psm0032@alu.medac.es>
2025-12-07 19:22:02 +0100

    Creada la clase para auditorias

---
+++ neobank-api/api/src/main/java/com/example/neo_bank/api/audit/Audit.java
@@ -0,0 +1,12 @@
+package com.example.neo_bank.api.audit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Audit {
+    String action();
+}
